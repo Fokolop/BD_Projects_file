d065c0afe26ff2a93018b875408d15ad168c7335
Sonikhahahah <136900495+Fokolop@users.noreply.github.com>
2025-02-10 20:40:28 +0300

    day00_ex05.sql

--- day00_ex05.sql
+++ day00_ex05.sql
@@ -1,4 +1,4 @@
-SELECT 
-(select name from person where id = person_order.person_id ) AS NAME 
-FROM person_order
-WHERE person_order.menu_id in (13, 14, 18) and person_order.order_date = ('2022-01-07')
+select
+(select name from person where id = person_order.person_id ) as NAME 
+from person_order
+where person_order.menu_id in (13, 14, 18) and person_order.order_date = ('2022-01-07')
