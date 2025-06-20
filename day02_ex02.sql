18df5fe1ea59e698d29715e50e61f0a37c27523b
Sonikhahahah <136900495+Fokolop@users.noreply.github.com>
2025-06-20 15:20:13 +0300

    day02_ex02.sql

---
+++ day02_ex02.sql
@@ -0,0 +1,5 @@
+select COALESCE(p.name, '-') as person_name, pv.visit_date, COALESCE(pz.name, '-') as pizzeria_name
+from ( select * from person_visits where visit_date between '2022-01-01' and '2022-01-03') as pv
+full join person p on pv.person_id = p.id
+full join pizzeria pz on pv.pizzeria_id = pz.id
+order by person_name, pv.visit_date;
